761200f19f8550f80ca4ae7878a8175c7dd4638a
X_V.neer <xv.neer@gmail.com>
2023-01-31 23:46:05 +0200

    setup the project using node + Ts and  vitest for testing

---
+++ dist/intex.js
@@ -0,0 +1,3 @@
+console.log("2");
+export {};
+//# sourceMappingURL=intex.js.map
---
+++ src/test/first.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("this is a test", () => {
+  it("should pass this test", () => {
+    expect(true).toBe(true);
+  });
+});
